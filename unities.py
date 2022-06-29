65ffaf70fa3cf5267c1e81034374152eed23cf90
Jiaxin Yu <9447954+JiaxinYu@users.noreply.github.com>
2022-06-29 17:01:23 +0800

    modified condition
    modified data inclusion criteria

--- unities.py
+++ unities.py
@@ -5,13 +5,17 @@ from pathlib import Path
 from datetime import datetime
 from pyteomics import mgf
 
-def params_pars(mgf_filename, filepath=rootpath):
+def params_pars(fname, fpath):
   # get pepmass and mz array for the following data pipeline
+  mgf_file = mgf.read(str(fpath/fname))
   pepmass = []
   mz_array = []
-  mgf_file = mgf.read(str(filepath/mgf_filename))
   for spectrum in mgf_file:
-    pepmass.append(spectrum.get('params').get('pepmass')[0])
-    mz_array.append(spectrum.get('m/z array'))
+    rtime = float(spectrum.get('params').get('title').split(' ')[3])
+    # keep data between 6 to 110 mins
+    if (rtime > 6) and (rtime < 110):
+      pepmass.append(spectrum.get('params').get('pepmass')[0])
+      mz_array.append(spectrum.get('m/z array'))
   mgf_file.close()
-  return mgf_file
+  
+  return pepmass, mz_array
